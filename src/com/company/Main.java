12adf276258ef2d2d8d506fff1f31e8be819bc3c
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-10-04 20:28:49 +0300

    Updated-4.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,64 +7,79 @@ import java.util.Random;
 public class Main
 {
     static int My_Array[];
-    static class My_Thread_1 extends Thread
+    static class My_Array_Thread extends Thread
     {
-        My_Thread_1(String name)
+        My_Array_Thread(String name)
         {
             super(name);
         }
-
         public void run()
         {
-            System.out.printf(Thread.currentThread().getName() + " started!\n");
             try
             {
-                Random random = new Random(); My_Array = new int[10];
-                for (int i = 0; i < My_Array.length; i++)
-                { My_Array[i] = random.nextInt(); }
-                for (int i = 0; i < My_Array.length; i++)
-                { System.out.print(My_Array[i] + " "); } System.out.print("\n");
+                Random random = new Random();
+                My_Array = new int[10];
+                for (int i = 0; i < My_Array.length; i++) { My_Array[i] = random.nextInt(); }
+                for (int i = 0; i < My_Array.length; i++) { System.out.print(My_Array[i] + " "); } System.out.print("\n");
 
                 Thread.sleep(500);
             }
             catch(InterruptedException e)
-            { System.out.println("Thread has been interrupted"); }
-            System.out.printf(Thread.currentThread().getName() + " fiished!\n");
+            {
+                System.out.println(e);
+            }
         }
     }
 
-    static class My_Thread_2 extends Thread
+    static class My_Average_Thread extends Thread
     {
-        My_Thread_2(String name)
+        My_Average_Thread(String name)
         {
             super(name);
         }
-
         public void run()
         {
-            System.out.printf(Thread.currentThread().getName() + " started!\n");
             try
             {
-                /*double average = 0;
-                if (My_Array.length > 0)
-                { double sum = 0;
-                for (int j = 0; j < My_Array.length; j++) { sum += My_Array[j]; }
-                average = sum / My_Array.length; }
-                System.out.println(average);*/
                 OptionalDouble count = IntStream.of(My_Array).average();
                 System.out.println(count);
+
                 Thread.sleep(500);
             }
             catch(InterruptedException e)
-            { System.out.println("Thread has been interrupted"); }
-            System.out.printf(Thread.currentThread().getName() + " fiished!\n");
+            {
+                System.out.println(e);
+            }
+        }
+    }
+
+    static class My_Average_Sum extends Thread
+    {
+        My_Average_Sum(String name)
+        {
+            super(name);
+        }
+        public void run()
+        {
+            try
+            {
+                int count = IntStream.of(My_Array).sum();
+                System.out.println(count);
+
+                Thread.sleep(500);
+            }
+            catch(InterruptedException e)
+            {
+                System.out.println(e);
+            }
         }
     }
 
     public static void main(String[] args) throws InterruptedException
     {
-        new My_Thread_1("My_Thread_1").start();
+        new My_Array_Thread("My_Array_Thread").start();
         Thread.sleep(1000);
-        new My_Thread_2("My_Thread_2").start();
+        new My_Average_Thread("My_Average_Thread").start();
+        new My_Average_Sum("My_Average_Sum").start();
     }
 }
