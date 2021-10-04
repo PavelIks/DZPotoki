b766587dd931398eb7c5ba31d21dbe1a63297be1
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-10-04 21:18:20 +0300

    Updated-9.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,21 +2,20 @@ package com.company;
 
 import java.util.Arrays;
 import java.util.stream.IntStream;
-/*import java.util.Random;*/
 
 public class Main
 {
     static int My_Array[];
+
     static class My_Array_Thread extends Thread
     {
         public void run()
         {
             try
             {
-                /*Random random = new Random();*/
                 My_Array = new int[20];
-                for (int i = 0; i < My_Array.length; i++) { /*My_Array[i] = random.nextInt();*/ My_Array[i] = (int)(Math.random()*11); }
-                for (int i = 0; i < My_Array.length; i++) { System.out.print(My_Array[i] + " "); } System.out.print("- массив (диапазон -- 0-10)" + "\n");
+                for (int i = 0; i < My_Array.length; i++) { My_Array[i] = (int)(Math.random()*11); }
+                for (int i = 0; i < My_Array.length; i++) { System.out.print(My_Array[i] + " "); } System.out.print("— массив (диапазон — 0-10)" + "\n");
 
                 Thread.sleep(500);
             }
@@ -34,7 +33,7 @@ public class Main
             try
             {
                 double avg = Arrays.stream(My_Array).average().orElse(Double.NaN);
-                System.out.println(avg + " - среднее арифметическое массива");
+                System.out.println(avg + " — среднее арифметическое массива");
 
                 Thread.sleep(500);
             }
@@ -52,7 +51,7 @@ public class Main
             try
             {
                 int count = IntStream.of(My_Array).sum();
-                System.out.println(count + " - сумма массива");
+                System.out.println(count + " — сумма массива");
 
                 Thread.sleep(500);
             }
